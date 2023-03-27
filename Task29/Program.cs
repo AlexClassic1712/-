b441b2e416be5b18d5e701c68011173678d68c80
Alex <Alex@Alex.ru>
2023-03-27 10:47:00 +0300

    DZ

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -5,12 +5,13 @@ Random rnd = new Random();
 for (int i = 0; i < size; i++)
 {
     arr[i] = rnd.Next(2, 7 + 1);
+    Console.Write($"{arr[i]} ");
 }
 PrintArray(arr);
 
 void PrintArray(int[] arr)
 {
-    Console.Write("[");
+    Console.Write("-> [");
     for (int i = 0; i < arr.Length; i++)
     {
         if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");

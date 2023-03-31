722dfcc8ac5e15ac39228a8e89e1fc9a78b1021b
Alex <Alex@Alex.ru>
2023-03-31 17:03:16 +0300

    DZ

---
+++ Task41/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите колличество чисел, которое хотите ввести:");
+int arrLenght = Convert.ToInt32(Console.ReadLine());
+
+Polojit(arrLenght);
+int Polojit(int size)
+{
+  int count=0;
+
+    int[] arr = new int[size];
+ Console.WriteLine("Введите произвольные целые числа:");
+    
+    for (int i = 0; i < size; i++)
+    {
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+   if (arr[i] > 0) count++;
+    }
+ Console.WriteLine($"Колличество положительных введенных чисел: {count}");
+return count;
+}
---
+++ Task43/Program.cs
@@ -0,0 +1,38 @@
+﻿//  Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// k1 = 5, b1 = 2, k2 = 9, b2 = 4 -> (-0,5; -0,5)
+
+
+Console.WriteLine("Введите коэффициент k1:");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите коэффициент b1:");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите коэффициент k2:");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите коэффициент b2:");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+if (k1 == k2 && b1 == b2)
+Console.WriteLine("Прямые совпадают");
+else if(k1 == k2 && b1 != b2)
+Console.WriteLine("Прямые параллельны");
+else PrintIntersectionPoint(IntersectionPoint(k1, b1, k2, b2));
+
+double[] IntersectionPoint(double k1, double b1, double k2, double b2)
+{
+  
+    double x =(b2 - b1)/(k1 - k2);
+    double y = k1 * x + b1;
+    
+    double mX = Math.Round(x,1, MidpointRounding.AwayFromZero);
+    double mY = Math.Round(y,1, MidpointRounding.AwayFromZero);
+    double[] arr = {mX, mY};
+
+    return arr;
+}
+ 
+ void PrintIntersectionPoint(double[] arr)
+ {
+    Console.WriteLine($"Точка пересечения прямых ({arr[0]} ; {arr[1]})");
+
+ }

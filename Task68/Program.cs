50de12bb4682beabea9c419d4c75ccd54f510c85
Alex <Alex@Alex.ru>
2023-04-12 16:02:11 +0300

    DZ

---
+++ Task68/Program.cs
@@ -0,0 +1,24 @@
+﻿void ShowNumbers(int firstNum, int secondNum)
+{
+    if (firstNum < secondNum)
+    {
+        ShowNumbers(firstNum, secondNum - 1);
+        Console.Write($"{secondNum} ");
+    }
+    else if (firstNum > secondNum)
+    {
+        Console.Write($"{firstNum} ");
+        ShowNumbers(firstNum - 1, secondNum);
+    }
+    else
+    {
+        Console.Write($"{firstNum} ");
+    }
+}
+
+Console.Write("Введите первое натуральное число: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе натуральное число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Числа от {numberM} до {numberN}: ");
+ShowNumbers(numberM, numberN);
